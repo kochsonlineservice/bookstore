aed62d8f8680e9f8e773fcb572c4a6d5a0135cb5
Ralph Koch <deine@email.de>
2026-06-08 17:29:51 +0200

    style.css-

--- js/template.js
+++ js/template.js
@@ -1,17 +1,30 @@
 function getBookTemplate(book) {
   return `
 
-              <div class="book_card">
-                          <div class="book_name">
-                              <h2 class="h2">${book.name}</h2> 
-                          </div>
+      <article class="book_card">
+
+<!-- book-title -->
+          <div class="book_name">
+                <h2 class="h2">${book.name}</h2> 
+          </div>
+
+<!-- book-img -->
+          <div class="img">
+              <img class="img_book"  src="${book.image}">
+          </div>
 
 
-                            <div class="img">
-                                <img class="img_book"  src="${book.image}">
-                            </div>
+<!-- book-price-and-likes -->      
+          <div class="book_price_like">
 
+              <p class="book_price">${book.price} €</p>
 
+            <div class="book_like">
+                <span>1249</span>
+                <span>❤️</span>
+            </div>
+
+          </div>
 
 
                       
@@ -19,13 +32,13 @@ function getBookTemplate(book) {
 
                         <p class="book_author">${book.author}</p>
 
-                      <p class="book_price">${book.price} €</p>
+                      
 
                       <p class="book_genre">${book.genre} </p>
 
 
 
-              </div>
+              </article>
         `;
 }
 
